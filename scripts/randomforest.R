091c891f304dcd675fe9263a9a0d533f607e7000
Javi <javiergalandiaz@gmail.com>
2022-11-18 17:15:30 +0100

    sclerophyllous vegetation

--- scripts/randomforest.R
+++ scripts/randomforest.R
@@ -13,45 +13,29 @@ str(df_GE_med)
 
 myvar <- colnames(df_GE_med)[4:16] # traits
 
-# remove SOSV y EOSV because of their correlation with MINV
-# remove EOSD y MAXD as they dont look important in the boxplots
-rf_var <- setdiff(myvar, c('SOSV','EOSV','EOSD','MAXD'))
-# remove category managed
-data_rf <- df_GE_med %>% dplyr::select(class, all_of(rf_var)) %>% subset(class != 'managed')
+# remove SOSV y EOSV because of their high correlation with MINV
+rf_var <- setdiff(myvar, c('SOSV','EOSV'))
+data_rf <- df_GE_med %>% dplyr::select(class, all_of(rf_var))
 # factorize classes
 data_rf$class <- data_rf$class %>% as.character() %>% as.factor()
 
-# get best ntry
-# control <- trainControl(method="repeatedcv", number=10, repeats=3, search="random")
-# rf_random <- train(class~., data=data_rf, method="rf", metric="Accuracy", tuneLength=15, trControl=control)
-# print(rf_random)
-# plot(rf_random)
+# 10 percent of data for external validation
+ind_in <- sample(1:nrow(data_rf), nrow(data_rf)/10)
+data_ex <- data_rf[ind_in,]
+data_rf <- data_rf[-ind_in,]
 
-d_sample = sample.split(data_rf$class, SplitRatio = .75)
-d_train = subset(data_rf, d_sample == TRUE)
-d_test  = subset(data_rf, d_sample == FALSE)
+# 5-fold cross validation 
+control <- trainControl(method="repeatedcv", number=5, repeats=5, search="random")
+rf_random <- train(class~., data=data_rf, method="rf", metric="Accuracy", trControl=control)
+# save(rf_random, file="results/rf_random.RData")
+rf_random
+plot(rf_random)
+varImp(rf_random, scale=F)
+plot(varImp(rf_random, scale=F))
 
-par(mfrow=c(1,1))
-rf1 <- randomForest(class~., data=d_train, ntree=1000, mtry=2, replace=T, importance=T)
-rf1
-plot(rf1)
-rf1$importance
-varImpPlot(rf1, main=NULL)
-
-pred = predict(rf1, newdata=d_test[-1]) # cross-validation
-(cm = table(d_test[,1], pred))
-
-# accuracy
-(sum(diag(cm))/sum(cm))
-
-# precision
-diag(cm)/colSums(cm)
-
-# recall
-diag(cm)/rowSums(cm)
-
-# F1
-2*(diag(cm)/colSums(cm) * diag(cm)/rowSums(cm))/(diag(cm)/colSums(cm) + diag(cm)/rowSums(cm))
+# confusion matrix
+predicted_class <- predict(rf_random, newdata=data_ex[,rf_var], type='raw')
+confusionMatrix(data=predicted_class, reference=data_ex$class)
 
 
 # classify broadleaf category from DLT
@@ -59,53 +43,52 @@ diag(cm)/rowSums(cm)
 # import dataset
 df_DLT_HRVPP <- read.csv("C:/Users/user/Desktop/CAPAS_ROI/df_DLT_HRVPP.txt", sep="")
 
-# filter broadleaf
-df_broadleaf <- df_DLT_HRVPP %>% filter(class=='broadleaf')
-df_broadleaf$class <- NULL
-
 # traits means for classification
-df_broadleaf_means <- df_broadleaf[,c('x','y')]
-df_broadleaf_means$LENG <- df_broadleaf[,c("LENG17","LENG18","LENG19","LENG20")] %>% rowMeans()
-df_broadleaf_means$LSLO <- df_broadleaf[,c("LSLO17","LSLO18","LSLO19","LSLO20")] %>% rowMeans()
-df_broadleaf_means$MAXV <- df_broadleaf[,c("MAXV17","MAXV18","MAXV19","MAXV20")] %>% rowMeans()
-df_broadleaf_means$MINV <- df_broadleaf[,c("MINV17","MINV18","MINV19","MINV20")] %>% rowMeans()
-df_broadleaf_means$RSLO <- df_broadleaf[,c("RSLO17","RSLO18","RSLO19","RSLO20")] %>% rowMeans()
-df_broadleaf_means$SOSD <- df_broadleaf[,c("SOSD17","SOSD18","SOSD19","SOSD20")] %>% rowMeans()
-df_broadleaf_means$SPRO <- df_broadleaf[,c("SPRO17","SPRO18","SPRO19","SPRO20")] %>% rowMeans()
-df_broadleaf_means$TPRO <- df_broadleaf[,c("TPRO17","TPRO18","TPRO19","TPRO20")] %>% rowMeans()
-df_broadleaf_means$AMPL <- df_broadleaf[,c("AMPL17","AMPL18","AMPL19","AMPL20")] %>% rowMeans()
+df_DLT_HRVPP_means <- df_DLT_HRVPP[,c('x','y','class')]
+df_DLT_HRVPP_means$LENG <- df_DLT_HRVPP[,c("LENG17","LENG18","LENG19","LENG20")] %>% rowMeans()
+df_DLT_HRVPP_means$LSLO <- df_DLT_HRVPP[,c("LSLO17","LSLO18","LSLO19","LSLO20")] %>% rowMeans()
+df_DLT_HRVPP_means$MAXV <- df_DLT_HRVPP[,c("MAXV17","MAXV18","MAXV19","MAXV20")] %>% rowMeans()
+df_DLT_HRVPP_means$MINV <- df_DLT_HRVPP[,c("MINV17","MINV18","MINV19","MINV20")] %>% rowMeans()
+df_DLT_HRVPP_means$RSLO <- df_DLT_HRVPP[,c("RSLO17","RSLO18","RSLO19","RSLO20")] %>% rowMeans()
+df_DLT_HRVPP_means$SOSD <- df_DLT_HRVPP[,c("SOSD17","SOSD18","SOSD19","SOSD20")] %>% rowMeans()
+df_DLT_HRVPP_means$SPRO <- df_DLT_HRVPP[,c("SPRO17","SPRO18","SPRO19","SPRO20")] %>% rowMeans()
+df_DLT_HRVPP_means$TPRO <- df_DLT_HRVPP[,c("TPRO17","TPRO18","TPRO19","TPRO20")] %>% rowMeans()
+df_DLT_HRVPP_means$AMPL <- df_DLT_HRVPP[,c("AMPL17","AMPL18","AMPL19","AMPL20")] %>% rowMeans()
+df_DLT_HRVPP_means$EOSD <- df_DLT_HRVPP[,c("EOSD17","EOSD18","EOSD19","EOSD20")] %>% rowMeans()
+df_DLT_HRVPP_means$MAXD <- df_DLT_HRVPP[,c("MAXD17","MAXD18","MAXD19","MAXD20")] %>% rowMeans()
+
 
 # assign classes using random forest model
-rownames(df_broadleaf_means) <- 1:nrow(df_broadleaf_means)
-pred1 <- predict(rf1, newdata=df_broadleaf_means[,rf_var], type='prob') %>% as.data.frame()
-pred1$class <- predict(rf1, newdata=df_broadleaf_means[,rf_var], type='class') %>% as.vector()
+rownames(df_DLT_HRVPP_means) <- 1:nrow(df_DLT_HRVPP_means)
+pred1 <- predict(rf_random, newdata=df_DLT_HRVPP_means[,rf_var], type='prob') %>% as.data.frame()
+pred1$class2 <- predict(rf_random, newdata=df_DLT_HRVPP_means[,rf_var], type='raw') %>% as.vector()
 # cbind
-pred1 <- cbind(pred1, as.vector(apply(pred1[c('deciduous','evergreen','golf')], 1, max)))
-colnames(pred1) <- c('deciduous','evergreen','golf','class','max')
-pred1 <- pred1[,c('class','max')]
-
-hist(pred1$max) # umbral aleatorio
-table(pred1$max > 0.95)
+pred1$max <- as.vector(apply(pred1[c('coniferous','sclerophyllous','deciduous','golf','managed')], 1, max))
+pred1 <- pred1[,c('class2','max')]
 
 # merge original data
-df_broadleaf_means <- cbind(df_broadleaf_means, pred1)
-df_broadleaf_means <- df_broadleaf_means %>% subset(max > 0.95)
+df_DLT_HRVPP_means <- cbind(df_DLT_HRVPP_means, pred1)
 
-# elimino golf
-df_broadleaf_means <- df_broadleaf_means %>% subset(class != 'golf')
-table(df_broadleaf_means$class)
+# compare classifications
+table(df_DLT_HRVPP_means$class, df_DLT_HRVPP_means$class2)
+
+# elimino golf y managed
+df_DLT_HRVPP_means <- df_DLT_HRVPP_means %>% subset(class2 %in% c('deciduous','coniferous','sclerophyllous'))
+
+# threshold
+hist(pred1$max) # umbral aleatorio
+table(pred1$max > 0.90)
+df_DLT_HRVPP_means <- df_DLT_HRVPP_means %>% subset(max > 0.90)
+
+# final data for analyses
+table(df_DLT_HRVPP_means$class2)
 
 # merge classification with yearly indexes
-df_broadleaf_classified <- merge(df_broadleaf, df_broadleaf_means[,c('x','y','class')], by=c('x','y'))
-
-# rbind with coniferous
-df_coniferous <- df_DLT_HRVPP %>% filter(class=='coniferous')
-df_broadleaf_classified
-df_DLT_HRVPP_classified <- rbind(df_coniferous, df_broadleaf_classified[,colnames(df_coniferous)])
+df_DLT_HRVPP_classified <- merge(df_DLT_HRVPP, df_DLT_HRVPP_means[,c('x','y','class','class2')], by=c('x','y','class'))
 
 # visual inspection
-out_ever <- df_DLT_HRVPP_classified %>% group_by(class) %>% sample_n(500) %>% dplyr::select(x,y,class) %>% as.data.frame()
-out_ever <- vect(out_ever, geom=c('x','y'), 'epsg:32630') %>% project('epsg:4326') %>% terra::as.data.frame(geom='XY')
+out_ever <- df_DLT_HRVPP_classified %>% group_by(class2) %>% sample_n(300) %>% dplyr::select(x,y,class2) %>% as.data.frame()
+out_ever <- vect(out_ever, geom=c('x','y'), 'epsg:32630') %>% terra::project('epsg:4326') %>% terra::as.data.frame(geom='XY')
 write.table(out_ever, 'results/out_ever.txt', row.names=F)
 
 # save
@@ -113,7 +96,7 @@ write.table(df_DLT_HRVPP_classified, 'results/df_DLT_HRVPP_classified.txt')
 
 
 # plot
-df_DLT_means_classified <- df_DLT_HRVPP_classified[,c('x','y','class')]
+df_DLT_means_classified <- df_DLT_HRVPP_classified[,c('x','y','class','class2')]
 df_DLT_means_classified$EOSD <- df_DLT_HRVPP_classified[,c("EOSD17","EOSD18","EOSD19","EOSD20")] %>% rowMeans()
 df_DLT_means_classified$EOSV <- df_DLT_HRVPP_classified[,c("EOSV17","EOSV18","EOSV19","EOSV20")] %>% rowMeans()
 df_DLT_means_classified$LENG <- df_DLT_HRVPP_classified[,c("LENG17","LENG18","LENG19","LENG20")] %>% rowMeans()
@@ -128,6 +111,6 @@ df_DLT_means_classified$SPRO <- df_DLT_HRVPP_classified[,c("SPRO17","SPRO18","SP
 df_DLT_means_classified$TPRO <- df_DLT_HRVPP_classified[,c("TPRO17","TPRO18","TPRO19","TPRO20")] %>% rowMeans()
 df_DLT_means_classified$AMPL <- df_DLT_HRVPP_classified[,c("AMPL17","AMPL18","AMPL19","AMPL20")] %>% rowMeans()
 
-long_df_DLT_means_classified <- df_DLT_means_classified %>% gather(4:16, key='trait', value='value')
-ggplot(aes(x=class, y=value), data=long_df_DLT_means_classified) + geom_boxplot() + facet_wrap(~trait, scales = "free_y")
+long_df_DLT_means_classified <- df_DLT_means_classified %>% gather(5:17, key='trait', value='value')
+ggplot(aes(x=class2, y=value), data=long_df_DLT_means_classified) + geom_boxplot() + facet_wrap(~trait, scales = "free_y")
 
--- scripts/training dataset.R
+++ scripts/training dataset.R
@@ -18,31 +18,30 @@ DLT <- rast('C:/Users/user/Desktop/CAPAS_ROI/DLT.tif')
 # write.csv(ss_STL, 'C:/Users/user/OneDrive/TESIS Y PUBLICACIONES/AIRTEC/Pollen_UrbanSources/data/ss_GE.csv', row.names = FALSE)
 
 # import manually classified training layers
-ss_GE <- read_excel("data/ss_GE.xlsx")
-ss_GE <- ss_GE[!is.na(ss_GE$class),]
 
-mykml <- st_read('data/polygons_v2.kml') %>% st_transform(32630)
+# 1
+# ss_GE <- read_excel("data/ss_GE.xlsx")
+# ss_GE <- ss_GE[!is.na(ss_GE$class),]
 
-k1 <- mykml[1,] %>% vect()
-ss_buff <- terra::extract(DLT, k1, method="simple", xy=T)
-ss_buff$class <- k1$Name
-ss_buff <- ss_buff %>% dplyr::select(x,y,class) 
+# 2
+mykml <- st_read('data/polygons_v3.kml') %>% st_transform(32630)
+list_kml <- list()
 for (i in 1:dim(mykml)[1]) {
   try(k1 <- mykml[i,] %>% vect())
   k2 <- terra::extract(DLT, k1, xy=T)
   k2$class <- k1$Name
   k2 <- k2 %>% dplyr::select(x,y,class)  
-  ss_buff <- rbind(ss_buff, k2)
-}; rm(k1,k2)
-
-# ss_buff <- na.omit(ss_buff)
-table(ss_buff$class)
+  list_kml[[i]] <- k2
+}
+df_kml <- do.call(rbind, list_kml)
+df_kml <- na.omit(df_kml)
+table(df_kml$class)
 
 # merge tables
-ss_GE <- ss_GE %>% dplyr::select(x,y,class)
-ss_GE <- rbind(ss_buff, ss_GE)
-ss_GE <- ss_GE %>% subset(class %in% c('deciduous','evergreen','golf','managed'))
-rm(ss_buff)
+# ss_GE <- ss_GE %>% dplyr::select(x,y,class)
+# ss_GE <- rbind(ss_buff, ss_GE)
+# ss_GE <- ss_GE %>% subset(class %in% c('deciduous','evergreen','golf','managed'))
+ss_GE <- df_kml
 # point file
 pt_GE <- vect(ss_GE, geom=c('x','y'), 'epsg:32630')
 
@@ -134,11 +133,6 @@ df_GE_med$SPRO <- df_GE[,c("SPRO17","SPRO18","SPRO19","SPRO20")] %>% rowMeans()
 df_GE_med$TPRO <- df_GE[,c("TPRO17","TPRO18","TPRO19","TPRO20")] %>% rowMeans()
 df_GE_med$AMPL <- df_GE[,c("AMPL17","AMPL18","AMPL19","AMPL20")] %>% rowMeans()
 
-# save
-write.table(df_GE, 'results/df_GE.txt')
-write.table(df_GE_med, 'results/df_GE_med.txt')
-
-
 # exploratory
 table(df_GE_med$class)
 
@@ -146,36 +140,44 @@ table(df_GE_med$class)
 myvar <- c("EOSD","EOSV","LENG","LSLO","MAXD","MAXV","MINV","RSLO","SOSD","SOSV","SPRO","TPRO","AMPL")
 
 # correlation
-pairs(df_GE_med[,myvar])
-
-# differences among groups
-long_GE_med <- df_GE_med %>% gather(4:16, key='trait', value='value')
-ggplot(aes(x=class, y=value), data=long_GE_med) + geom_boxplot() + facet_wrap(~trait, scales = "free_y")
+# pairs(df_GE_med[,myvar])
 
 # visual inspection
-out_ever <- df_GE_med[,c('x','y','class')]
-out_ever <- vect(out_ever, geom=c('x','y'), 'epsg:32630') %>% project('epsg:4326') %>% terra::as.data.frame(geom='XY')
-write.table(out_ever, 'results/out_ever.txt', row.names=F)
+# out_ever <- df_GE_med[,c('x','y','class')]
+# out_ever <- vect(out_ever, geom=c('x','y'), 'epsg:32630') %>% project('epsg:4326') %>% terra::as.data.frame(geom='XY')
+# write.table(out_ever, 'results/out_ever.txt', row.names=F)
 
-# correct points
-df_GE_med$class[which(df_GE_med$class=='evergreen' & df_GE_med$LSLO>0.03)] <- 'deciduous'
-df_GE_med$class[which(df_GE_med$class=='golf' & df_GE_med$EOSD<200)] <- 'managed'
+# correct class
+df_GE_med$class <- as.factor(df_GE_med$class)
+levels(df_GE_med$class) <- c("coniferous","deciduous","sclerophyllous","golf","managed")
+df_GE_med$class <- factor(df_GE_med$class, levels=c("coniferous","sclerophyllous","deciduous","golf","managed"))
+
+# correct points after visual inspection
+df_GE_med$class[which(df_GE_med$class=='deciduous' & df_GE_med$AMPL<0.35)] <- NA
 df_GE_med$class[which(df_GE_med$class=='deciduous' & df_GE_med$SOSD<60)] <- NA
-df_GE_med$class[which(df_GE_med$class=='evergreen' & df_GE_med$EOSD<200)] <- NA
+df_GE_med$class[which(df_GE_med$class=='deciduous' & df_GE_med$LSLO<0.0075)] <- NA
+df_GE_med$class[which(df_GE_med$class=='golf' & df_GE_med$EOSD<200)] <- 'managed'
 df_GE_med$class[which(df_GE_med$class=='golf' & df_GE_med$MAXV<1.5)] <- NA
-df_GE_med$class[which(df_GE_med$class=='deciduous' & df_GE_med$AMP<0.5)] <- NA
-df_GE_med$class[which(df_GE_med$class=='evergreen' & df_GE_med$MAXV>1.75)] <- NA
-df_GE_med$class[which(df_GE_med$class=='deciduous' & df_GE_med$LSLO<0.01)] <- NA
+df_GE_med$class[which(df_GE_med$class=='sclerophyllous' & df_GE_med$AMPL>1.25)] <- NA
+df_GE_med$class[which(df_GE_med$class=='coniferous' & df_GE_med$LSLO>0.03)] <- 'deciduous'
+df_GE_med$class[which(df_GE_med$class=='sclerophyllous' & df_GE_med$LSLO>0.03)] <- 'deciduous'
+df_GE_med$class[which(df_GE_med$class=='coniferous' & df_GE_med$MAXV>1.75)] <- NA
+df_GE_med$class[which(df_GE_med$class=='sclerophyllous' & df_GE_med$MAXV>1.75)] <- NA
+df_GE_med$class[which(df_GE_med$class=='coniferous' & df_GE_med$EOSD<250)] <- NA
+
+# differences among groups
+long_GE_med <- df_GE_med %>% gather(4:16, key='trait', value='value') %>% na.omit()
+ggplot(aes(x=class, y=value), data=long_GE_med) +
+  geom_boxplot() +
+  facet_wrap(~trait, scales = "free_y") +
+  theme(axis.text.x=element_text(angle = -45, hjust = 0))
 
 # remove NAs from means df
 df_GE_med <- na.omit(df_GE_med)
 
-# last inspection
-long_GE_med <- df_GE_med %>% gather(4:16, key='trait', value='value')
-ggplot(aes(x=class, y=value), data=long_GE_med) + geom_boxplot() + facet_wrap(~trait, scales = "free_y")
-
 # add classes to original dataframe
-df_GE <- inner_join(df_GE_med[,c('x','y','class')], df_GE, by=c('x','y'))
+df_GE$class <- NULL # because we changed some classes after inspection
+df_GE <- left_join(df_GE_med[,c('x','y','class')], df_GE, by=c('x','y'))
 
 # save
 write.table(df_GE, 'results/df_GE.txt')
