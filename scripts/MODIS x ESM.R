8c4396e379bc7cdb433e5e9ea27ff2893a068be8
Javi <javiergalandiaz@gmail.com>
2022-12-10 14:14:22 +0100

    update

--- scripts/MODIS x ESM.R
+++ scripts/MODIS x ESM.R
@@ -2,6 +2,7 @@
 library(sf)
 library(car)
 library(GGally)
+library(ggpubr)
 source('scripts/study_area.R')
 
 
@@ -29,7 +30,7 @@ MODIS_DAY <- MODIS_DAY %>% terra::crop(STUDY_AREA, mask=T)
 
 # ESM ~ MODIS
 # dataframe
-ESM_rs <- terra::resample(x=ESM, y=MODIS_AVERAGE, method='sum')
+ESM_rs <- terra::resample(x=ESM, y=MODIS_DAY, method='sum')
 
 ESMxMOD <- merge(terra::as.data.frame(ESM_rs, xy=T), terra::as.data.frame(MODIS_NIGHT, xy=T),
                  by=c('x','y'))
@@ -63,27 +64,28 @@ par(mfrow=c(2,2), mar=c(5,5,5,5)); plot(mod)
 
 a1 <- ggplot(aes(x=ESM/max(ESM)*100,y=Night), data=ESMxMOD) +
   geom_point() +
-  geom_smooth(method='lm', level=0.95) +
+  geom_smooth(formula = y~poly(x, 2, raw=TRUE), method='lm', level=0.95) +
   labs(y='Night LST Spring (ºC) ', x='Percentage of built-up area') +
   theme_classic()
 
 # plots from 'MODIS'
 
-# df_mod_date <- read.csv("C:/Users/user/OneDrive/TESIS Y PUBLICACIONES/AIRTEC/madrid_urbantrees/results/df_mod_date.txt", sep="")
-# temp2 <- df_mod_date %>% pivot_longer(cols=2:3, names_to='passing', values_to='LST')
-# temp2$passing <- as.factor(temp2$passing); levels(temp2$passing) <- c('Day','Night')
-# temp2$date <- as.Date(temp2$date, "%Y-%m-%d")
-# a2 <- ggplot(aes(y=LST, x=date, group=passing, colour=passing), data=temp2) +
-#   geom_point(shape=1) + theme_classic() +
-#   geom_smooth(method='loess', span=0.1) +
-#   labs(y=expression(paste(Delta, ' LST (ºC)', sep=' ')), x=' ') +
-#   theme(legend.position=c(0.80,0.95), legend.title=element_blank(), legend.direction="horizontal") +
-#   geom_hline(yintercept=c(0,mean(df_mod_date$LST_day),mean(df_mod_date$LST_night)), linetype=c('solid',"dashed","dashed"), color=c('black','#F8766D','#619CFF')) +
-#   geom_vline(xintercept= c(
-#     as.numeric(as.Date("2018-01-01", "%Y-%m-%d")),
-#     as.numeric(as.Date("2019-01-01", "%Y-%m-%d")),
-#     as.numeric(as.Date("2020-01-01", "%Y-%m-%d"))), linetype=4, colour="black") 
-  
-# ggarrange(a2,a1, labels=c('(a)','(b)'),
-#           nrow=1,ncol=2,widths=c(1,1))   
+df_mod_date <- read.csv("C:/Users/user/OneDrive/TESIS Y PUBLICACIONES/AIRTEC/madrid_urbantrees/results/df_mod_date.txt", sep="")
+df_mod_date <- df_mod_date %>% filter(date < '2020-12-31')
+temp2 <- df_mod_date %>% pivot_longer(cols=2:3, names_to='passing', values_to='LST')
+temp2$passing <- as.factor(temp2$passing); levels(temp2$passing) <- c('Day','Night')
+temp2$date <- as.Date(temp2$date, "%Y-%m-%d")
+a2 <- ggplot(aes(y=LST, x=date, group=passing, colour=passing), data=temp2) +
+  geom_point(shape=1) + theme_classic() +
+  geom_smooth(method='loess', span=0.1) +
+  labs(y=expression(paste(Delta, ' LST (ºC)', sep=' ')), x=' ') +
+  theme(legend.position=c(0.80,0.95), legend.title=element_blank(), legend.direction="horizontal") +
+  geom_hline(yintercept=c(0,mean(df_mod_date$LST_day),mean(df_mod_date$LST_night)), linetype=c('solid',"dashed","dashed"), color=c('black','#F8766D','#619CFF')) +
+  geom_vline(xintercept= c(
+    as.numeric(as.Date("2018-01-01", "%Y-%m-%d")),
+    as.numeric(as.Date("2019-01-01", "%Y-%m-%d")),
+    as.numeric(as.Date("2020-01-01", "%Y-%m-%d"))), linetype=4, colour="black")
+
+ggarrange(a2,a1, labels=c('(a)','(b)'),
+          nrow=1,ncol=2,widths=c(0.9,1))
 
